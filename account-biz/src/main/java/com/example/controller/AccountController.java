3e8529ca31474ac5d6e60caba2749ff307ddd56f
bujiatang0423 <bujiatang0423@gmail.com>
2025-03-27 15:54:26 +0800

    更新 fegin 的目录结构

--- account-biz/src/main/java/com/example/controller/AccountController.java
+++ account-biz/src/main/java/com/example/controller/AccountController.java
@@ -8,6 +8,7 @@ import com.example.service.AccountService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +21,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class AccountController implements AccountApi {
 
     private final AccountService accountService;
+
+    @PostMapping("/insert")
     @Override
     public ResultData<String> insert(@RequestBody  AccountDTO accountDTO) {
         log.info("12312312312");
